da3b904b181907383f1737a8bde21d5ba8132841
hobo0xcc <hobo0xcc@gmail.com>
2022-04-12 21:38:08 +0900

    el1

--- src/arch/aarch64/start.rs
+++ src/arch/aarch64/start.rs
@@ -1,3 +1,5 @@
+use core::arch::asm;
+
 extern "C" {
     pub fn main();
 }
@@ -5,6 +7,34 @@ extern "C" {
 #[no_mangle]
 #[cfg(target_arch = "aarch64")]
 pub unsafe extern "C" fn start() {
-    main();
+    asm!(
+        "
+        mov x0, #0x4b1
+        msr scr_el3, x0
+        adr x0, main
+        msr elr_el3, x0
+        mov x0, #0x5
+        msr spsr_el3, x0
+        mov x0, sp
+        msr sp_el1, x0
+
+        mov x0, 0
+        msr sctlr_el1, x0
+        ldr x0, =(1 << 31)
+        msr hcr_el2, x0
+        eret
+    "
+    );
+
+    /*
+    #define MODE_AARCH64_EL2H 0x9
+    mov   x0, #0x4b1    // RW=1, SMD=1, RES=1, NS=1
+    msr   scr_el3, x0
+    adr   x0, start_el2
+    msr   elr_el3, x0
+    mov   x0, #MODE_AARCH64_EL2H
+    msr   spsr_el3, x0
+    eret
+    */
     loop {}
 }
